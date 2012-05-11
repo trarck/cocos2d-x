11384455900584ac55d9222d7490b660767f81f5
Rolando Abarca <m@rolando.cl>
2012-05-11 12:02:43 -0700

    more comprehensive example

--- js/test_bindings/simple_class.h
+++ js/test_bindings/simple_class.h
@@ -1,3 +1,6 @@
+#ifndef __SIMPLE_CLASS_H__
+#define __SIMPLE_CLASS_H__
+
 #include <string>
 
 class SimpleNativeClass
@@ -11,11 +14,22 @@ public:
 	SimpleNativeClass();
 	~SimpleNativeClass();
 
-	int getSomeField();
-	int getSomeOtherField();
-	char *getAnotherMoreComplexField();
-	void setSomeField(int f);
-	void setSomeOtherField(int f);
+	// these methods are simple, can be defined inline
+	int getSomeField() {
+		return m_someField;
+	};
+	int getSomeOtherField() {
+		return m_someOtherField;
+	};
+	char *getAnotherMoreComplexField() {
+		return m_anotherMoreComplexField;
+	}
+	void setSomeField(int f) {
+		m_someField = f;
+	}
+	void setSomeOtherField(int f) {
+		m_someOtherField = f;
+	}
 	void setAnotherMoreComplexField(const char *str);
 
 	// std::string not working yet!
@@ -33,10 +47,17 @@ public:
 	AnotherClass();
 	~AnotherClass();
 
-	int getJustOneField();
+	// also simple methods, can be defined inline
+	int getJustOneField() {
+		return justOneField;
+	}
 	// wrong setter - won't work (needs ONLY one parameter in order to work)
-	void setJustOneField();
+	void setJustOneField() {
+		justOneField = 999;
+	}
 
 	void doSomethingSimple();
 };
 };
+
+#endif

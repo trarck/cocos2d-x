f0b29661be04e9e16a27c2687874c7a698c35590
houhai.duan <houhai.duan@happyelements.com>
2015-06-19 11:45:56 +0800

    use public for constructor

--- cocos/base/ccConfig.h
+++ cocos/base/ccConfig.h
@@ -305,7 +305,8 @@ THE SOFTWARE.
  * protected by default.
  */
 #ifndef CC_CONSTRUCTOR_ACCESS
-#define CC_CONSTRUCTOR_ACCESS protected
+//#define CC_CONSTRUCTOR_ACCESS protected
+#define CC_CONSTRUCTOR_ACCESS public
 #endif
 
 /** @def CC_ENABLE_ALLOCATOR
